c7ef641954dbe5b21466edd622268a0aa5870c50
valeriojiang <jiangvalerio1998@gmail.com>
2024-11-17 17:47:25 +0100

    refactor ArubaMiddleware to encapsulate token management and improve request handling

--- BE/src/app/middleware/client_cache_middleware.py
+++ BE/src/app/middleware/client_cache_middleware.py
@@ -6,27 +6,29 @@ import datetime
 import httpx
 from starlette.datastructures import MutableHeaders
 
-aruba_login_url = "https://login.aruba.it/auth/realms/cmp-new-apikey/protocol/openid-connect/token"
-aruba_client_id = "cmp-8542d30e-1cec-4f41-928a-52f4263c555a"
-aruba_client_secret = "qpVOYrJ6clZEJSngiQNiDAv8KajarBTu"
-aruba_token = ""
 
-aruba_base_url = "https://api.arubacloud.com"
-
-async def get_token():
-    url = aruba_login_url
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    data = {
-            'grant_type': 'client_credentials',
-            'client_id': aruba_client_id,
-            'client_secret': aruba_client_secret
-        }
-
-    async with httpx.AsyncClient() as client:
-        response = await client.post(url, headers=headers, data=data)
-    return response.json()
 
 class ArubaMiddleware(BaseHTTPMiddleware):
+    aruba_login_url = "https://login.aruba.it/auth/realms/cmp-new-apikey/protocol/openid-connect/token"
+    aruba_client_id = "cmp-8542d30e-1cec-4f41-928a-52f4263c555a"
+    aruba_client_secret = "qpVOYrJ6clZEJSngiQNiDAv8KajarBTu"
+    aruba_token = ""
+
+    aruba_base_url = "https://api.arubacloud.com"
+
+    async def get_token(self):
+        url = self.aruba_login_url
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        data = {
+            'grant_type': 'client_credentials',
+            'client_id': self.aruba_client_id,
+            'client_secret': self.aruba_client_secret
+        }
+
+        async with httpx.AsyncClient() as client:
+            response = await client.post(url, headers=headers, data=data)
+        return response.json()
+
     def __init__(self, app: FastAPI, max_age: int = 60) -> None:
         super().__init__(app)
         self.max_age = max_age
@@ -34,37 +36,24 @@ class ArubaMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
         
         response: Response = Response()
-        
+        current_aruba_token = self.aruba_token
         
         if request.url.path.startswith("/api/v1/aruba"):
             # update request headers
+            #current aruba_token we neew to check if it is expired or is null
+            if current_aruba_token == "" :
+                logging.info(f"Token is empty : {current_aruba_token}")
+                current_aruba_token = await self.get_token()
+                self.aruba_token = current_aruba_token['access_token']
+            elif jwt.decode(current_aruba_token.split(" ")[1], options={"verify_signature": False})["exp"] < datetime.datetime.now():
+                logging.info(f"Token is expired : {current_aruba_token}")
+                current_aruba_token = await self.get_token()
+                self.aruba_token = current_aruba_token['access_token']
+            
+            logging.info(f"Token is : {self.aruba_token}")  
             headers = dict(request.scope['headers'])
-            
-            
-            logging.info(f"Intercepted a request to {request.url.path}")
-            auth_header = request.headers.get("Authorization")
-            logging.info("Bearer :" + auth_header.split(" ")[1])
-
-            try:
-                decoded_token = jwt.decode(auth_header.split(" ")[1], options={"verify_signature": False})  # Adjust the algorithm as needed
-                expiration_date = datetime.datetime.fromtimestamp(decoded_token["exp"])
-                if expiration_date < datetime.datetime.now():
-                    token = await get_token()
-                    headers['Authorization'] = token
-                    request.scope['headers'] = [(k, v) for k, v in headers.items()]
-                    logging.info("Token has expired. new one is:")
-                    logging.info(token)
-
-                logging.info("Token is valid until:", expiration_date)  
-            except jwt.InvalidTokenError:
-                logging.error("Invalid token.")
-            response: Response = await call_next(request)
-            # Modify the response by adding a custom header
-            response.headers["X-Custom-Header"] = "This is a custom response header"
-
-        # Process the request and get the response
-        # Intercept the request to check for a Bearer token
-        else:
+            headers['Authorization'] = f"Bearer {self.aruba_token}"
+            request.scope['headers'] = [(k, v) for k, v in headers.items()]
             response: Response = await call_next(request)
 
         return response
